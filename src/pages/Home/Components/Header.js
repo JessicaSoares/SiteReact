a0522cb109bf53903320c9d7ab91e23643ebfae4
jess <jessicassoarez@gmail.com>
2021-01-13 02:08:17 -0300

    alterações toolbar

--- src/pages/Home/Components/Header.js
+++ src/pages/Home/Components/Header.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import {Bell} from 'react-feather';
+
 
 const useStyles = makeStyles({
-
+    AppBar: {
+        boxShadow:'none',
+    },
+    img:{
+        maxHeight: 55,
+    },
+    grow:{
+        flexGrow: 1,
     
-})
+    },
+    userSection:{
+        display:'flex',
+        alignItems:'center',
 
-function Header(){
+    },
+    button:{
+        paddingRight:10
+    }
+    
+});
+
+function Header() {
+    const classes = useStyles();
 return (
-    <header className = 'header'>
-    <div className = 'toolbar' >
-    <div>
-       <a href='/'> Conecta dev- Alteração </a>
-    </div>
-    <div>
-    <Button variant="contained" color="primary">Novo post</Button>
-        <span>img1</span>
-        <span>img2 </span>
-        </div>
-    </div>
-    </header>
-    );
+
+     <AppBar position="fixed" color="inherit" className={classes.AppBar}>
+         <Toolbar>
+             <img src = "/images/logo.png" alt = "logo" className = {classes.img} />
+             <div className = {classes.grow}> </div>
+             <div className = {classes.userSection}> </div>
+            <Button variant = "contained" color = "primary">
+            Novo post
+
+            </Button>
+            <SvgIcon>
+                <Bell></Bell>
+            </SvgIcon>
+
+             
+
+        {/* <div>
+       <a href="/"> Conecta dev- Alteração </a>
+       <input type = "text"></input>
+        </div> */}
+
+        </Toolbar>
+    </AppBar>
+    )
 }
+
 export default Header;
