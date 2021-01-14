84778c13f0712c04e3edd354f79fc2cc015549a4
jess <jessicassoarez@gmail.com>
2021-01-14 00:47:57 -0300

    Estilizando a navbar

--- src/pages/Home/Components/NavBar.js
+++ src/pages/Home/Components/NavBar.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
 // eslint-disable-next-line
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme) => ({
     root:{
-
-
+        padding: theme.spacing(2),
+        widht: 275,
+    },
+    button:{
+        width: '100%'
     }
 
-})
+}));
+
+
+
 function NavBar(){
+    const classes = useStyles();
 
 return(
-    <Paper>
-        NavBar
+    <Paper className = {classes.root}>
+        <Button variant = "outlined" color = "secondary" className = {classes.button}>Registrar grátis</Button>
     </Paper>
  )
 }
