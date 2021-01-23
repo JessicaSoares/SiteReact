13dc978436649a31c71efa1a5f03a616e40315cc
jess <jessicassoarez@gmail.com>
2021-01-23 16:03:06 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import SignIn from './pages/SingIn/index.js';
 import Home from './pages/Home/index.js';
 import { ThemeProvider } from '@material-ui/core/styles';
-
 import theme from './theme';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
-
-  console.log(window.location.href);
-  const url = window.location.href
 
     return (
       <ThemeProvider theme={theme}>
-        
+        <BrowserRouter>
+        <Routes>
+
+         <Route path = "/" element={<Home />}/>
+         <Route path = "/entrar" element ={<SignIn />}/>
+         <Route path = "*" element ={<h1> Not Found 404</h1>}/>
+        </Routes>
+        </BrowserRouter>
+        {/*
+          url === 'http://localhost:3000/'
+          ?<Home />
+          : <SignIn />
+        */}
           
-        <Home />
-        <SignIn /> 
-      
       </ThemeProvider>
     );
 }
--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SignIn(){
 return (
 
-    <hq>Login component</hq>
+    <h1>Login component</h1>
 )
 
 }
